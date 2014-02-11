dfa7d8f38e0f5d18920c4e2dfdd78cf7187f97f2
Tom Jacques <tejacques@gmail.com>
2014-02-11 14:40:16 -0500

    Update copylibs

--- copylibs.sh
+++ copylibs.sh
@@ -12,15 +12,5 @@ cp src/CallR/bin/Release/CallR.dll lib/nuget/CallR/
 cp src/CallR/bin/Release/CallR.pdb lib/nuget/CallR/
 cp src/CallR/bin/Release/CallR.xml lib/nuget/CallR/
 
-
-cp src/CallR/Scripts/jquery.signalR-2.0.1.js lib/bower/
-cp src/CallR/Scripts/jquery.signalR-2.0.1.min.js lib/bower/
-cp src/CallR/Scripts/jquery.signalR-2.0.1.min.js.map lib/bower/
-
-cp src/CallR/Scripts/callr.js lib/bower/
-cp src/CallR/Scripts/callr.min.js lib/bower/
-cp src/CallR/Scripts/callr.min.js.map lib/bower/
-
-cp src/CallR/Scripts/callr.angular.js lib/bower/
-cp src/CallR/Scripts/callr.angular.min.js lib/bower/
-cp src/CallR/Scripts/callr.angular.min.js.map lib/bower/
+cp src/CallR/Scripts/callr.js ../bower-callr/
+cp src/CallR/Scripts/callr.angular.js ../bower-callr.angular/
