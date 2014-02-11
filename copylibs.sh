a0193c3f81f5609c221987e999dddde7c6d202ff
Tom Jacques <tejacques@gmail.com>
2014-02-11 15:15:09 -0500

    Update copylibs to reflect new bower packages

--- copylibs.sh
+++ copylibs.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 cp src/CallR/Scripts/callr.js lib/nuget/CallR.JS/
-cp src/CallR/Scripts/callr.min.js lib/nuget/CallR.JS/
-cp src/CallR/Scripts/callr.min.js.map lib/nuget/CallR.JS/
-
 cp src/CallR/Scripts/callr.angular.js lib/nuget/CallR.Angular/
-cp src/CallR/Scripts/callr.angular.min.js lib/nuget/CallR.Angular/
-cp src/CallR/Scripts/callr.angular.min.js.map lib/nuget/CallR.Angular/
 
 cp src/CallR/bin/Release/CallR.dll lib/nuget/CallR/
 cp src/CallR/bin/Release/CallR.pdb lib/nuget/CallR/
