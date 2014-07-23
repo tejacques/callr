6c7ede5f272b248f69f535926bf5878d1fb550bb
Tom Jacques <tejacques@gmail.com>
2014-07-23 18:28:03 -0400

    Update libs and nuget packages

--- src/CallR.Sample/Scripts/callr.angular.js
+++ src/CallR.Sample/Scripts/callr.angular.js
@@ -1,5 +1,5 @@
 ﻿/*!
-* callr JavaScript Library v1.1.0 AngularJS Module
+* callr JavaScript Library v1.1.1 AngularJS Module
 * https://github.com/tejacques/callr
 *
 * Distributed in whole under the terms of the MIT License (MIT)
--- src/CallR.Sample/Scripts/callr.js
+++ src/CallR.Sample/Scripts/callr.js
@@ -1,5 +1,5 @@
 ﻿/*!
-* callr JavaScript Library v1.1.1
+* callr JavaScript Library v1.1.2
 * https://github.com/tejacques/callr
 *
 * Distributed in whole under the terms of the MIT License (MIT)
